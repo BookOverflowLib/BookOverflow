09059e2b6aeffef1f2657b61b62791662d766aa5
Alessandro Bernardello <53372753+alessandroberna@users.noreply.github.com>
2024-11-29 23:34:07 +0100

    SQL schema edits
    - Renamed `Utente` table to `User`
    - Added `password_salt` column to the `User` table
    - Renamed `Libri` table to `Libro`
    - Added `dataProposta` and `DataConclusione` columns to the `Scambio` table

--- tables.sql
+++ tables.sql
@@ -1,15 +1,14 @@
--- Table: Utente
 CREATE TABLE Utente (
     email VARCHAR(255) PRIMARY KEY,
-    password_hash VARCHAR(255) NOT NULL,
+    password_hash VARCHAR(255) NOT NULL, -- Password hashing will be done in PHP
+    password_salt VARCHAR(255) NOT NULL, 
     nome VARCHAR(100),
     cognome VARCHAR(100),
     citta VARCHAR(100)
 );
 
--- Table: Libri
-CREATE TABLE Libri (
-    isbn CHAR(13) PRIMARY KEY, -- ISBN is typically 13 characters
+CREATE TABLE Libro (
+    isbn CHAR(13) PRIMARY KEY,
     titolo VARCHAR(255) NOT NULL,
     autore VARCHAR(255),
     editore VARCHAR(255),
@@ -18,40 +17,44 @@ CREATE TABLE Libri (
     lingua VARCHAR(50)
 );
 
--- Table: Copia
 CREATE TABLE Copia (
     ID INT PRIMARY KEY AUTO_INCREMENT,
     ISBN CHAR(13),
-    condizioni VARCHAR(255),
-    disponibile BOOLEAN DEFAULT TRUE, -- Derived column for simplicity
+    disponibile BOOLEAN DEFAULT TRUE,
 
-    FOREIGN KEY (ISBN) REFERENCES Libri(isbn)
+    FOREIGN KEY (ISBN) REFERENCES Libro(isbn)
+    --condizioni VARCHAR(255),   -- Dobbiamo decidere i valori dell'ENUM
 );
 
--- Table: Desiderati
 CREATE TABLE Desiderati (
     email VARCHAR(255),
     ISBN CHAR(13),
 
     PRIMARY KEY (email, ISBN),
     FOREIGN KEY (email) REFERENCES Utente(email),
-    FOREIGN KEY (ISBN) REFERENCES Libri(isbn)
+    FOREIGN KEY (ISBN) REFERENCES Libro(isbn)
 );
 
--- Table: Posseduti
 CREATE TABLE Posseduti (
     email VARCHAR(255),
     IdCopia INT,
 
-    PRIMARY KEY (email, IdCopia),
+    PRIMARY KEY (IdCopia),  -- Credo convenga lasciare solo IDcopia come chiave primaria, per evitare che Scambio abbia una chiave primaria ancora più lunga
     FOREIGN KEY (email) REFERENCES Utente(email),
     FOREIGN KEY (IdCopia) REFERENCES Copia(ID)
 );
 
--- Table: Scambio
 CREATE TABLE Scambio (
     emailProponente VARCHAR(255),
     emailAccettatore VARCHAR(255),
     IdCopia1 INT,
     IdCopia2 INT,
-    data
+    dataProposta DATE, 
+    DataConclusione DATE, 
+
+    PRIMARY KEY (emailProponente, emailAccettatore, IdCopia1, IdCopia2, dataProposta),
+    FOREIGN KEY (emailProponente) REFERENCES Utente(email),
+    FOREIGN KEY (emailAccettatore) REFERENCES Utente(email),
+    FOREIGN KEY (IdCopia1) REFERENCES Copia(ID),
+    FOREIGN KEY (IdCopia2) REFERENCES Copia(ID)
+);

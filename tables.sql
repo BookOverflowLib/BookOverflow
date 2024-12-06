6826d6f4d2ecc20d1e06e3153d0a412beebdc4f2
lucaribon <lucaribon0203@gmail.com>
2024-12-06 15:24:51 +0100

    refactor: tables.sql; feaat: tables.sql; fix: index.html placeholder to comment

--- tables.sql
+++ tables.sql
@@ -1,9 +1,10 @@
 CREATE TABLE
     Utente (
         email VARCHAR(255) PRIMARY KEY,
-        password_hash VARCHAR(255) NOT NULL, -- Password hashing will be done in PHP
+         -- Password hashing will be done in PHP
+        password_hash VARCHAR(255) NOT NULL,
         password_salt VARCHAR(255) NOT NULL,
-        username VARCHAR(100) NOT NULL,
+        username VARCHAR(100) UNIQUE NOT NULL,
         nome VARCHAR(100),
         cognome VARCHAR(100),
         citta VARCHAR(100)
@@ -11,11 +12,11 @@ CREATE TABLE
 
 CREATE TABLE
     Libro (
-        isbn CHAR(13) PRIMARY KEY,
+        ISBN CHAR(13) PRIMARY KEY,
         titolo VARCHAR(255) NOT NULL,
         autore VARCHAR(255),
         editore VARCHAR(255),
-        anno INT,
+        anno YEAR,
         genere VARCHAR(100),
         lingua VARCHAR(50)
     );
@@ -23,52 +24,63 @@ CREATE TABLE
 CREATE TABLE
     Copia (
         ID INT PRIMARY KEY AUTO_INCREMENT,
-        ISBN CHAR(13),
-        disponibile BOOLEAN DEFAULT TRUE,
-        FOREIGN KEY (ISBN) REFERENCES Libro (isbn) condizioni ENUM (
+        ISBN CHAR(13) NOT NULL,
+        proprietario VARCHAR(255) NOT NULL,
+        -- utile se un utente vuole mettere tutta la lista dei suoi libri nella piattaforma perché gli altri possano vedere che libri ha, senza però che risultino disponibili per essere scambiati
+        disponibile BOOLEAN DEFAULT TRUE, 
+        condizioni ENUM (
             'nuovo',
             'come nuovo',
             'usato ma ben conservato',
             'usato',
             'danneggiato'
         ),
+        FOREIGN KEY (ISBN) REFERENCES Libro (ISBN),
+        FOREIGN KEY (proprietario) REFERENCES Utente (email)
     );
 
 CREATE TABLE
-    Desiderati (
+    Desiderio (
         email VARCHAR(255),
         ISBN CHAR(13),
         PRIMARY KEY (email, ISBN),
         FOREIGN KEY (email) REFERENCES Utente (email),
-        FOREIGN KEY (ISBN) REFERENCES Libro (isbn)
-    );
-
-CREATE TABLE
-    Posseduti (
-        email VARCHAR(255),
-        idCopia INT,
-        PRIMARY KEY (eamil, idCopia),
-        FOREIGN KEY (email) REFERENCES Utente (email),
-        FOREIGN KEY (idCopia) REFERENCES Copia (ID)
+        FOREIGN KEY (ISBN) REFERENCES Libro (ISBN)
     );
 
 CREATE TABLE
     Scambio (
+        -- ID al posto di pk(emailProponente, emailAccettatore, idCopiaProp, idCopiaAcc, dataProposta)
+        ID INT PRIMARY KEY AUTO_INCREMENT,
         emailProponente VARCHAR(255),
         emailAccettatore VARCHAR(255),
         idCopiaProp INT,
         idCopiaAcc INT,
-        dataProposta DATE,
-        DataConclusione DATE,
-        PRIMARY KEY (
-            emailProponente,
-            emailAccettatore,
-            IdCopiaProp,
-            IdCopiaAcc,
-            dataProposta
-        ),
+        dataProposta DATE DEFAULT CURRENT_DATE,
+        dataConclusione DATE,
         FOREIGN KEY (emailProponente) REFERENCES Utente (email),
         FOREIGN KEY (emailAccettatore) REFERENCES Utente (email),
         FOREIGN KEY (idCopiaProp) REFERENCES Copia (ID),
         FOREIGN KEY (idCopiaAcc) REFERENCES Copia (ID)
+    );
+
+CREATE TABLE
+    Recensione (
+        -- email del recensito derivata dalla tabella scambio
+        emailRecensore VARCHAR(255),
+        idScambio INT,
+        dataPubblicazione DATE DEFAULT CURRENT_DATE, 
+        valutazione TINYINT CHECK (valutazione >= 1 AND valutazione <= 5),
+        contenuto TEXT, -- 65k caratteri
+        FOREIGN KEY (emailRecensore) REFERENCES Utente (email),
+        FOREIGN KEY (idScambio) REFERENCES Scambio (ID)
+    );
+
+CREATE TABLE
+    Follow (
+        emailSeguace VARCHAR(255),
+        emailSeguito VARCHAR(255),
+        PRIMARY KEY (emailSeguito, emailSeguace),
+        FOREIGN KEY (emailSeguace) REFERENCES Utente (email),
+        FOREIGN KEY (emailSeguito) REFERENCES Utente (email)
     );

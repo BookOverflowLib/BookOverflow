3a9f15bd1245969ace6f5ce1df0cc38578ee4d16
Alessandro Bernardello <53372753+alessandroberna@users.noreply.github.com>
2024-12-13 21:09:50 +0100

    refactor: make queries use prepared statements

--- src/model/dbAPI.php
+++ src/model/dbAPI.php
@@ -41,7 +41,7 @@ class DBAccess
         //$this->connection = mysqli_connect(DBAccess::HOST_DB, DBAccess::USERNAME, DBAccess::PASSWORD, DBAccess::DATABASE_NAME);
 
         // errors check in debug
-        return mysqli_connect_error();
+        return mysqli_connect_error();  // ???
 
         // errors check in production
         // if(mysqli_connect_errno()) {
@@ -60,6 +60,44 @@ class DBAccess
         mysqli_close($this->connection);
     }
 
+    private function prepare_and_execute_query($query, $types = null, $params = null) : ?array
+    {
+        if (!$this->connection) {
+            $this->open_connection();
+        }
+
+        $stmt = $this->connection->prepare($query);
+        if (!$stmt) {
+            throw new Exception("Prepare failed: " . $this->connection->error);
+        }
+
+        if ($types && $params) {
+            // there must be the same number of types and parameters
+            // e.g types = "iss", params = [1, "hello", 3.14]
+            if (strlen($types) !== count($params)) {
+                throw new Exception("Number of types does not match number of parameters");
+            }
+            
+            // Bind parameters dynamically
+            if (!$stmt->bind_param($types, ...$params)) {
+                throw new Exception("Parameter binding failed: " . $stmt->error);
+            }
+        }
+    
+        if (!$stmt->execute()) {
+            throw new Exception("Execute failed: " . $stmt->error);
+        }
+
+        $result = $this->query_results_to_array($stmt->get_result());
+        
+        $stmt->close();
+        $this->close_connection(); 
+        // we might want to keep the connection open
+        // constant opening and closing is slow
+        
+        return $result;
+    }
+
     public function get_most_traded_with_cover($limit)
     {
         $query = "SELECT L.ISBN, L.titolo, L.autore, I.url, COUNT(*) AS numero_vendite
@@ -71,52 +109,24 @@ class DBAccess
                     WHERE I.isCopertina = TRUE
                     GROUP BY L.ISBN, L.titolo, L.autore, I.url
                     ORDER BY numero_vendite DESC
-                    LIMIT " . $limit;
-        $queryRes = mysqli_query($this->connection, $query or die(mysqli_error($this->connection)));
-
-        if (mysqli_num_rows($queryRes) == 0) {
-            return null;
-        } else {
-            $res = array();
-            while ($row = mysqli_fetch_assoc($queryRes)) {
-                array_push($res, $row);
-            }
-            $queryRes->free();
-            return $res;
-        }
+                    LIMIT ?";
+        
+        return $this->prepare_and_execute_query($query, "i", [$limit]);
     }
 
-    // public function insertNewElement()
-    // {
-    //     // TODO: usare prepare statement
-    //     $queryIns = "";
-    //     $queryRes = mysqli_query($this->connection, $queryIns or die(mysqli_error($this->connection)));
+    function get_citta_by_provincia($provincia): ?array
+    {
+        $query = "SELECT * FROM citta WHERE provincia = ?";
+        return $this->prepare_and_execute_query($query, "s", [$provincia]);
+    }
 
-    //     if (mysqli_affected_rows($this->connection) > 0) {
-    //         return true;
-    //     } else {
-    //         return false;
-    //     }
-    // }
-
+    // No need for prepared statements? 
     function get_province(): ?array
     {
         $this->open_connection();
 
         $query = "SELECT id, nome FROM province";
 
-        $queryRes = mysqli_query($this->connection, $query);
-        $this->handle_query_error($queryRes);
-
-        $this->close_connection();
-
-        return $this->query_results_to_array($queryRes);
-    }
-    function get_citta_by_provincia($provincia): ?array
-    {
-        $this->open_connection();
-
-        $query = "SELECT * FROM citta WHERE provincia = '$provincia'";
         $queryRes = mysqli_query($this->connection, $query);
         $this->handle_query_error($queryRes);
 

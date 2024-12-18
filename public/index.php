d8138c0fa12252e956f8cc24becb0f33b9ae9d3b
frguzzo <158739119+frguzzo@users.noreply.github.com>
2024-12-18 16:21:08 +0100

    feat: update Docker configuration and enhance HTML structure with dynamic navbar

--- public/index.php
+++ public/index.php
@@ -8,6 +8,7 @@ $footer = file_get_contents('./html/footer.html');
 $db = new DBAccess();
 $dbOK = $db->open_connection();
 
+$mostTradedCoversHTML = '';
 if ($dbOK) {
     $mostTradedCovers = $db->get_most_traded_with_cover("4");
     foreach ($mostTradedCovers as $cover) {
@@ -22,6 +23,26 @@ if ($dbOK) {
     // TODO: gestire errore connesione
 }
 
-echo str_replace('<!--[header]-->', $header, $index);
-echo str_replace('<!--[footer]-->', $footer, $index);
-echo str_replace('<!--[mostTraded]-->', $mostTradedCoversHTML, $index);
+$currentPage = $_SERVER['REQUEST_URI'];
+
+$navbarReferences = array(
+    array('href' => '/', 'text' => 'Home'),
+    array('href' => '/esplora', 'text' => 'Esplora'),
+    array('href' => '/contatti', 'text' => 'Contatti'),
+    array('href' => '/profilo', 'text' => 'Profilo')
+);
+
+$li = '';
+foreach ($navbarReferences as $ref) {
+    if ($currentPage != $ref['href']) {
+        $li .= '<li class="active"><a href="' . $ref['href'] . '">' . $ref['text'] . '</a></li>';
+    } else {
+
+        $li .= '<li>' . $ref['text'] . '</li>';
+    }
+}
+$header = str_replace('<!-- [navbar] -->', $li, $header);
+$index = str_replace('<!-- [header] -->', $header, $index);
+$index = str_replace('<!-- [footer] -->', $footer, $index);
+$index = str_replace('<!-- [mostTraded] -->', $mostTradedCoversHTML, $index);
+echo $index;

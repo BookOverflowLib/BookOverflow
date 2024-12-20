b5e2b89ce0dedb6cbc7a601944e1ebce2fb0f0e0
bazz333 <matteobazzan333@gmail.com>
2024-12-20 16:03:07 +0100

    feat: aggiunti select provincia e comune dinamici

--- public/registrati.php
+++ public/registrati.php
@@ -1,4 +1,5 @@
 <?php
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $username = $_POST['username'];
     $password = $_POST['password'];
@@ -8,23 +9,24 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
 require_once '../src/model/dbAPI.php';
 require_once '../src/model/utils.php';
-
+require_once '../src/model/registrationSelect.php';
 
 $db = new DBAccess();
-$dbOK = $db->open_connection();
 
-
-$PAGE_TITLE = "Accedi - BookOverflow";
+$PAGE_TITLE = "Registrati - BookOverflow";
 
 $template = file_get_contents('./html/templatePage.html');
 $header = getHeaderSection();
-$accedi = file_get_contents('./html/accedi.html');
+$registrati = file_get_contents('./html/registrati.html');
 $footer = file_get_contents('./html/footer.html');
+
+$provinceList = optionProvince();
+$registrati = str_replace('<!-- [province] -->', $provinceList, $registrati);
 
 $page = str_replace('<!-- [pageTitle] -->', $PAGE_TITLE, $template);
 $page = str_replace('<!-- [header] -->', $header, $page);
 $page = str_replace('<!-- [footer] -->', $footer, $page);
-$page = str_replace('<!-- [content] -->', $accedi, $page);
+$page = str_replace('<!-- [content] -->', $registrati, $page);
 echo $page;
 
 if (isset($_POST['username'], $_POST['password'])) {

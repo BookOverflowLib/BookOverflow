c3158ff170bd7a8623b337c9bd19a65fbdeb01b1
bazz333 <matteobazzan333@gmail.com>
2024-12-27 19:08:04 +0100

    feat: aggiunta funzione per ottenere l'URL dell'immagine utente e aggiornamento della registrazione utente

--- src/model/dbAPI.php
+++ src/model/dbAPI.php
@@ -1,119 +1,131 @@
 <?php
+
 // TODO: maybe exception could be handled using best practices but i don´t think it's a requirement
 class DBAccess
 {
-    private const HOST_DB = "db";
-    private const DATABASE_NAME = "bookoverflow";
-    private const USERNAME = "test";
-    private const PASSWORD = "test";
+	private const HOST_DB = "db";
+	private const DATABASE_NAME = "bookoverflow";
+	private const USERNAME = "test";
+	private const PASSWORD = "test";
 
-    private $connection;
+	private $connection;
 
-    public function __construct()
-    {
-        $this->open_connection();
-    }
+	public function __construct()
+	{
+		$this->open_connection();
+	}
 
-    public function __destruct()
-    {
-        $this->close_connection();
-    }
+	public function open_connection()
+	{
+		// turn on the errors | convert those errors into exceptions
+		mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 
-    public function open_connection()
-    {
-        // turn on the errors | convert those errors into exceptions
-        mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+		try {
+			$this->connection = new mysqli(
+				DBAccess::HOST_DB,
+				DBAccess::USERNAME,
+				DBAccess::PASSWORD,
+				DBAccess::DATABASE_NAME
+			);
+		} catch (mysqli_sql_exception $e) {
+			throw new Exception("Connection error: " . $e->getMessage());
+		}
 
-        try {
-            $this->connection = new mysqli(
-                DBAccess::HOST_DB,
-                DBAccess::USERNAME,
-                DBAccess::PASSWORD,
-                DBAccess::DATABASE_NAME
-            );
-        } catch (mysqli_sql_exception $e) {
-            throw new Exception("Connection error: " . $e->getMessage());
-        }
+		// errors check in debug; returns the error message from the last connection attempt
+		return mysqli_connect_error();
 
-        // errors check in debug; returns the error message from the last connection attempt
-        // return mysqli_connect_error();
+		// errors check in production
+		// if(mysqli_connect_errno()) {
+		// 	return false;
+		// } else {
+		// 	return true;
+		// }
+	}
 
-        // errors check in production
-        if(mysqli_connect_errno()) {
-            return false;
-        } else {
-            return true;
-        }
-    }
+	public function __destruct()
+	{
+		$this->close_connection();
+	}
 
-    public function close_connection()
-    {
-        if ($this->connection) {
-            mysqli_close($this->connection);
-            $this->connection = null;
-        }
-    }
-
-    public function ensure_connection(): void
-    {
-        if (!$this->connection) {
-            $this->open_connection();
-        }
-    }
-
-    public function prepare_and_execute_query($query, $types = null, $params = null): ?array
-    {
-        $this->ensure_connection();
-
-        $stmt = $this->connection->prepare($query);
-        if (!$stmt) {
-            throw new Exception("Prepare failed: " . $this->connection->error);
-        }
-
-        if ($types && $params) {
-            // there must be the same number of types and parameters
-            // e.g types = "iss", params = [1, "hello", 3.14]
-            if (strlen($types) !== count($params)) {
-                throw new Exception("Number of types does not match number of parameters");
-            }
-
-            // Bind parameters dynamically
-            if (!$stmt->bind_param($types, ...$params)) {
-                throw new Exception("Parameter binding failed: " . $stmt->error);
-            }
-        }
-
-        if (!$stmt->execute()) {
-            throw new Exception("Execute failed: " . $stmt->error);
-        }
-
-        $result = $this->query_results_to_array($stmt->get_result());
-
-        $stmt->close();
-        // we might want to keep the connection open
-        // constant opening and closing is slow
-        $this->close_connection();
-
-        return $result;
-    }
-
-    private function query_results_to_array($queryRes): ?array
-    {
-        if (mysqli_num_rows($queryRes) == 0) {
-            return null;
-        }
-        $res = array();
-        while ($row = mysqli_fetch_assoc($queryRes)) {
-            array_push($res, $row);
-        }
-        $queryRes->free();
-        return $res;
-    }
+	public function close_connection()
+	{
+		if ($this->connection) {
+			mysqli_close($this->connection);
+			$this->connection = null;
+		}
+	}
 
 
-    public function get_most_traded_with_cover($limit)
-    {
-        $query = "SELECT L.ISBN, L.titolo, L.autore, I.path, COUNT(*) AS numero_vendite
+	public function ensure_connection(): void
+	{
+		if (!$this->connection) {
+			$this->open_connection();
+		}
+	}
+
+	/**
+	 * Forse è troppo cursata :(
+	 * se è un SELECT ritorna un array,
+	 * se è un INSERT o qualsiasi altra cosa ritorna bool
+	 */
+	public function prepare_and_execute_query($query, $types = null, $params = null): array|bool
+	{
+		$this->ensure_connection();
+
+		$stmt = $this->connection->prepare($query);
+		if (!$stmt) {
+			throw new Exception("Prepare failed: " . $this->connection->error);
+		}
+
+		if ($types && $params) {
+			// there must be the same number of types and parameters
+			// e.g types = "iss", params = [1, "hello", 3.14]
+			if (strlen($types) !== count($params)) {
+				throw new Exception("Number of types does not match number of parameters");
+			}
+
+			// Bind parameters dynamically
+			if (!$stmt->bind_param($types, ...$params)) {
+				throw new Exception("Parameter binding failed: " . $stmt->error);
+			}
+		}
+
+		if (!$stmt->execute()) {
+			throw new Exception("Execute failed: " . $stmt->error);
+		}
+
+		$results = "";
+		if (str_starts_with($query, "SELECT")) {
+			$results = $this->query_results_to_array($stmt->get_result());
+		} else {
+			if (!$stmt->get_result() && !$stmt->errno()) {
+				$results = true;
+			} else {
+				$results = false;
+			}
+		}
+
+		$stmt->close();
+
+		$this->close_connection();
+
+		return $results;
+	}
+
+	public function get_province(): ?array
+	{
+		$this->ensure_connection();
+		$query = "SELECT id, nome FROM province ORDER BY nome";
+		$queryRes = mysqli_query($this->connection, $query);
+
+		$this->close_connection();
+
+		return $this->query_results_to_array($queryRes);
+	}
+
+	public function get_most_traded_with_cover($limit)
+	{
+		$query = "SELECT L.ISBN, L.titolo, L.autore, I.url, COUNT(*) AS numero_vendite
                     FROM Scambio AS S 
                     JOIN Copia AS CProp ON S.idCopiaProp = CProp.ID
                     JOIN Copia AS CAcc ON (S.idCopiaAcc = CAcc.ID AND CProp.ID != CAcc.ID) 
@@ -124,45 +136,47 @@ class DBAccess
                     ORDER BY numero_vendite DESC
                     LIMIT ?";
 
-        return $this->prepare_and_execute_query($query, "i", [$limit]);
-    }
+		return $this->prepare_and_execute_query($query, "i", [$limit]);
+	}
 
-    function get_comune_by_provincia($idProvincia): ?array
-    {
-        $query = "SELECT * FROM comuni WHERE id_provincia = ?";
-        try {
-            return $this->prepare_and_execute_query($query, "s", [$idProvincia]);
-        } catch (Exception $e) {
-            echo $e->getMessage();
-        }
-        return null;
-    }
+	public function get_comune_by_provincia($idProvincia): ?array
+	{
+		$query = "SELECT * FROM comuni WHERE id_provincia = ?";
+		try {
+			return $this->prepare_and_execute_query($query, "s", [$idProvincia]);
+		} catch (Exception $e) {
+			echo $e->getMessage();
+		}
+		return null;
+	}
 
-    public function get_province(): ?array
-    {
-        $this->ensure_connection();
-        $query = "SELECT id, nome FROM province ORDER BY nome";
-        $queryRes = mysqli_query($this->connection, $query);
+	public function register_user($nome, $cognome, $provincia, $comune, $email, $username, $password, $profileImg = null): bool
+	{
+		$passwordHashed = password_hash($password, PASSWORD_BCRYPT);
 
-        $this->close_connection();
+		$query = "INSERT INTO Utente (email, password_hash, username, nome, cognome, provincia, comune, path_immagine) VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
+		try {
+			return $this->prepare_and_execute_query($query, "ssssssss", [$email, $passwordHashed, $username, $nome, $cognome, $provincia, $comune, $profileImg]);
 
-        return $this->query_results_to_array($queryRes);
-    }
+		} catch (Exception $e) {
+			echo $e->getMessage();
+		}
+		return false;
+	}
 
-    // ritorna un true se l'utente è stato aggiunto
-    public function register_user($nome, $cognome, $provincia, $comune, $email, $username, $password, $profileImg = null): bool
-    {
-        $passwordHashed = password_hash($password, PASSWORD_BCRYPT);
+	private function query_results_to_array($queryRes): ?array
+	{
+		if (mysqli_num_rows($queryRes) == 0) {
+			return null;
+		}
+		$res = array();
+		while ($row = mysqli_fetch_assoc($queryRes)) {
+			array_push($res, $row);
+		}
+		$queryRes->free();
+		return $res;
+	}
 
-        $query = "INSERT INTO Utente (email, password_hash, username, nome, cognome, provincia, comune, path_immagine) VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
-        try {
-            $res = $this->prepare_and_execute_query($query, "ssssssss", [$email, $passwordHashed, $username, $nome, $cognome, $provincia, $comune, $profileImg]);
-            if ($res) {
-                return true;
-            }
-        } catch (Exception $e) {
-            echo $e->getMessage();
-        }
-        return false;
-    }
+	// ritorna un true se l'utente è stato aggiunto
+
 }

e2d5c0da74880d6fe40df73c1f43530a8fe38dae
lucaribon <lucaribon0203@gmail.com>
2024-12-29 14:34:17 +0100

    copiato reigstrati.php

--- src/view/registrati.php
+++ src/view/registrati.php
@@ -1,27 +1,15 @@
 <?php
-require_once '../src/model/dbAPI.php';
-require_once '../src/model/utils.php';
-require_once '../src/model/registrationSelect.php';
+require_once '../src/paths.php';
+require_once $GLOBALS['MODEL_PATH'] . 'dbAPI.php';
+require_once $GLOBALS['MODEL_PATH'] . 'utils.php';
+require_once $GLOBALS['MODEL_PATH'] . 'registration-select.php';
 
-$db = new DBAccess();
 
-$PAGE_TITLE = "Registrati - BookOverflow";
-
-$template = file_get_contents('./html/templatePage.html');
-$header = getHeaderSection();
-$registrati = file_get_contents('./html/registrati.html');
-$footer = file_get_contents('./html/footer.html');
+$page = getTemplatePage('Registrati');
+$registrati = file_get_contents($GLOBALS['TEMPLATES_PATH'] . 'registrati.html');
 
 $provinceList = optionProvince();
 $registrati = str_replace('<!-- [province] -->', $provinceList, $registrati);
-
-$page = str_replace('<!-- [pageTitle] -->', $PAGE_TITLE, $template);
-$page = str_replace('<!-- [header] -->', $header, $page);
-$page = str_replace('<!-- [footer] -->', $footer, $page);
 $page = str_replace('<!-- [content] -->', $registrati, $page);
-echo $page;
 
-if (isset($_POST['username'], $_POST['password'])) {
-    echo "username: " . $_POST['username'] . "<br>";
-    echo "password: " . $_POST['password'] . "<br>";
-}
+echo $page;

9a06da17e6e3acd728441e6c6fd9d49a42d0bd16
lucaribon <lucaribon0203@gmail.com>
2024-12-29 02:04:21 +0100

    feat: progressi con il controllo di registrati ma ancora da sistemare

--- public/js/checkerRegistrati.js
+++ public/js/checkerRegistrati.js
@@ -1,40 +1,49 @@
+// [0]: hint
+// [1]: regex
+// [2]: error
+// [3]: isValid
 var formChecks = {
     nome: [
         "Ex: Mario",
         /^[A-Za-z]{2,50}$/,
         "Inserire un nome di lunghezza almeno 2 e massimo 50, non sono ammessi numeri o caratteri speciali",
+        false
     ],
     cognome: [
         "Ex: Rossi",
         /^[A-Za-z' ]{2,50}$/,
         "Inserire un cognome di lunghezza almeno 2 e massimo 50, non sono ammessi numeri o caratteri speciali",
+        false
     ],
     // all characters are allowed
     username: [
         "Ex: MarioRossi",
-        /^[^\s\r\n]{1,50}$/,
+        /^[^\s\r\n]{2,50}$/,
         "Inserire un username di lunghezza almeno 2 e massimo 50, sono ammessi caratteri speciali e numeri",
+        false
     ],
     email: [
         "Ex: mariorossi@gmail.com",
         /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
         "Inserire un'email valida",
+        false
     ],
     password: [
         "Ex: Password123",
         /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()\-+=])[A-Za-z\d!@#$%^&*()\-+=]{8,20}$/,
         "La password deve essere lunga tra 8 e 20 caratteri, contenere almeno una lettera maiuscola (A-Z), una lettera minuscola (a-z), un numero (0-9) e un carattere speciale tra i seguenti: !@#$%^&*()-+=.",
+        false
     ],
     conferma: [
         "Ex: Password123",
         /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()\-+=])[A-Za-z\d!@#$%^&*()\-+=]{8,20}$/,
         "La password deve essere lunga tra 8 e 20 caratteri, contenere almeno una lettera maiuscola (A-Z), una lettera minuscola (a-z), un numero (0-9) e un carattere speciale tra i seguenti: !@#$%^&*()-+=.",
+        false
     ]
 };
 
 window.onload = function fillSuggestion() {
     for (var id in formChecks) {
-        console.log(id);
         var input = document.getElementById(id);
         setSuggestion(input, 0);
         input.onblur = function () {
@@ -43,38 +52,6 @@ window.onload = function fillSuggestion() {
     }
 }
 
-function checkRegex(input) {
-    // TODO: adapt
-    var regex = formChecks[input.id][1];
-    var text = input.value;
-
-    // return value of search: the index of the first match between the regular expression and the given string, or -1 if no match was found
-    if (text.search(regex) != 0) {
-        // tolgo suggerimento o errore precedente
-        var parent = input.parentNode;
-        parent.removeChild(parent.lastChild.previousSibling.previousSibling);
-
-        setSuggestion(input, 1);
-        input.focus();
-        input.select();
-        return false;
-    }
-    return true;
-}
-
-// TODO: maybe useless
-// function checkForm() {
-//     var form = document.forms[0];
-//     var inputs = form.getElementsByTagName("input");
-
-//     for (var i = 0; i < inputs.length; i++) {
-//         if (!checkRegex(inputs[i])) {
-//             return false;
-//         }
-//     }
-//     return true;
-// }
-
 /* 
 * mode = 0, modalità suggerimento
 * mode = 1, modalità errore
@@ -82,8 +59,8 @@ function checkRegex(input) {
 */
 function setSuggestion(input, mode) {
     var parent = input.parentNode;
-    console.log(parent);
-    var newNode = document.createElement("p");
+    var newNode = document.createElement("span");
+    newNode.id = input.id + "-sugg";
 
     switch (mode) {
         case 0:
@@ -103,4 +80,55 @@ function setSuggestion(input, mode) {
     }
 
     parent.insertBefore(newNode, parent.lastChild.previousSibling);
+}
+
+function checkRegex(input) {
+    // TODO: isValid true but it shouldn't??? maybe checkForm is not working properly
+    var regex = formChecks[input.id][1];
+    var text = input.value;
+    var suggestionElement = document.getElementById(input.id + "-sugg");
+
+    if (!input.value) {
+        formChecks[input.id][3] = false;
+        if (suggestionElement) {
+            suggestionElement.remove();
+        }
+        setSuggestion(input, 2);
+        input.focus();
+        input.select();
+    } else if (text.search(regex) != 0) {
+        formChecks[input.id][3] = false;
+        if (suggestionElement) {
+            suggestionElement.remove();
+        }
+        setSuggestion(input, 1);
+        input.focus();
+        input.select();
+    } else if (!formChecks[input.id][3]) {
+        formChecks[input.id][3] = true;
+        if (suggestionElement) {
+            suggestionElement.remove();
+        }
+    }
+}
+
+function checkForm() {
+    var form = document.getElementById("form-registrati");
+    var inputs = form.getElementsByTagName("input");
+
+    for (var i = 0; i < inputs.length; i++) {
+        if (!formChecks[inputs[i].id][3]) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+function checkPasswordConfirm() {
+    // TODO: implement
+    var password = document.getElementById("password");
+    var conferma = document.getElementById("conferma");
+
+    return true;
 }

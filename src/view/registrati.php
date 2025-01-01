4613b07280c06fb1b02e455861c6ab73e1b617b8
lucaribon <lucaribon0203@gmail.com>
2025-01-02 00:50:59 +0100

    merge conflicts 2

--- src/view/registrati.php
+++ src/view/registrati.php
@@ -11,3 +11,5 @@ $registrati = file_get_contents($GLOBALS['TEMPLATES_PATH'] . 'registrati.html');
 $provinceList = optionProvince();
 $registrati = str_replace('<!-- [province] -->', $provinceList, $registrati);
 $page = str_replace('<!-- [content] -->', $registrati, $page);
+
+echo $page;

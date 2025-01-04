1148c2a6c9e56444a138c688f518574686272693
bazz333 <matteobazzan333@gmail.com>
2025-01-04 19:56:53 +0100

    feat: pulsante hamburger accessibile; miglioramento gestione menu

--- public/js/hamburgerMenu.js
+++ public/js/hamburgerMenu.js
@@ -1,22 +1,57 @@
-let icon = document.getElementById('hamburger-icon')
-let menu = document.getElementById('hamburger-menu')
-let button = document.getElementById('hamburger')
-let header = document.getElementsByClassName('header-container')[0]
+function hamburgerMenu() {
+    const hamburger = document.getElementById('hamburger');
+    const hamburgerChiuso = document.querySelectorAll('#hamburger > span')[0];
+    const hamburgerAperto = document.querySelectorAll('#hamburger > span')[1];
 
-button.addEventListener('click', () => {
-    console.log('click')
-    if (menu.classList.contains('active') && icon.classList.contains('active')) {
-        if(header.classList.contains('activeHeader') && window.scrollY < 30) {
-            header.classList.remove('activeHeader')
+
+    let menu = document.getElementById('hamburger-menu')
+    let button = document.getElementById('hamburger')
+    let header = document.getElementsByClassName('header-container')[0]
+
+    hamburger.addEventListener('click', function () {
+        hamburgerChiuso.classList.toggle('active');
+        hamburgerAperto.classList.toggle('active');
+
+        hamburger.ariaPressed === "true" ? hamburger.ariaPressed = "false" : hamburger.ariaPressed = "true";
+
+        if (menu.classList.contains('active')) {
+            if (header.classList.contains('active') && window.scrollY < 30) {
+                header.classList.remove('active')
+            }
+            menu.classList.remove('active')
+            header.classList.remove('hamburger-active')
+        } else {
+            if (!header.classList.contains('active')) {
+                header.classList.add('active')
+            }
+            menu.classList.add('active')
         }
-        menu.classList.remove('active')
-        icon.classList.remove('active')
-        header.classList.remove('hamburger-active')
-    }else {
-        if (!header.classList.contains('activeHeader')) {
-            header.classList.add('activeHeader')
-        }
-        menu.classList.add('active')
-        icon.classList.add('active')
-    }
-})
+    })
+}
+
+hamburgerMenu();
+
+// let icon = document.getElementById('hamburger-icon')
+// let menu = document.getElementById('hamburger-menu')
+// let button = document.getElementById('hamburger')
+// let header = document.getElementsByClassName('header-container')[0]
+
+// button.addEventListener('click', () => {
+//     console.log('click')
+//     if (menu.classList.contains('active') && icon.classList.contains('active')) {
+//         if(header.classList.contains('active') && window.scrollY < 30) {
+//             header.classList.remove('active')
+//         }
+//         menu.classList.remove('active')
+//         icon.classList.remove('active')
+//         header.classList.remove('hamburger-active')
+//     }else {
+//         if (!header.classList.contains('active')) {
+//             header.classList.add('active')
+//         }
+//         menu.classList.add('active')
+//         icon.classList.add('active')
+//     }
+// })
+
+

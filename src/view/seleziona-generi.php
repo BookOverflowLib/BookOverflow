aec168a5d143d8509f3d8fef1efe11149c20266a
bazz333 <matteobazzan333@gmail.com>
2025-01-05 15:02:21 +0100

    feat: refactor styling for buttons and update genre selection template

--- src/view/seleziona-generi.php
+++ src/view/seleziona-generi.php
@@ -22,7 +22,7 @@ foreach ($fileGeneri as $key => $value) {
 
 $page = getTemplatePage("Impostazioni profilo");
 
-$generi = file_get_contents($GLOBALS['TEMPLATES_PATH'] . 'scegli-generi.html');
+$generi = file_get_contents($GLOBALS['TEMPLATES_PATH'] . 'seleziona-generi.html');
 
 $page = str_replace('<!-- [content] -->', $generi, $page);
 $page = str_replace('<!-- [opzioniGeneri] -->', $buttonsGeneri, $page);

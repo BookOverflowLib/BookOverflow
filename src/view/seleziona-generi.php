3df1880293ad883f3543b2c51eb4ec10bd3cfa40
bazz333 <matteobazzan333@gmail.com>
2025-01-05 14:17:58 +0100

    feat: seleziona-generi correct path; fix: path of assets in various files

--- src/view/seleziona-generi.php
+++ src/view/seleziona-generi.php
@@ -3,13 +3,21 @@ require_once '../src/paths.php';
 require_once $GLOBALS['MODEL_PATH'] . 'dbAPI.php';
 require_once $GLOBALS['MODEL_PATH'] . 'utils.php';
 
+if (!isset($_SESSION['user'])) {
+    session_start();
+}
+if ($_GET['user'] != $_SESSION['user']) {
+    header('Location: /profilo/' . $_GET['user']);
+    exit;
+}
+
 $fileGeneri = file_get_contents('../utils/bisac.json');
 $fileGeneri = json_decode($fileGeneri, true);
 
 
 $buttonsGeneri = '';
 foreach ($fileGeneri as $key => $value) {
-    $buttonsGeneri .= '<button type="button" class="button-genere" aria-pressed="false"><span aria-hidden="true">'. $value['emoji']."</span> ".$value['name'].'</button>';
+    $buttonsGeneri .= '<button type="button" class="button-genere" aria-pressed="false"><span aria-hidden="true">' . $value['emoji'] . "</span> " . $value['name'] . '</button>';
 }
 
 $page = getTemplatePage("Impostazioni profilo");

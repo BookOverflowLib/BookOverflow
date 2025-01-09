0ddcb64aec6b2dd65bf552e502dc46ac74e72a37
bazz333 <matteobazzan333@gmail.com>
2025-01-09 01:52:03 +0100

    feat: aggiunta funzionalità per aggiungere e rimuovere libri

--- src/view/libri-desiderati.php
+++ src/view/libri-desiderati.php
@@ -5,9 +5,8 @@ require_once $GLOBALS['MODEL_PATH'] . 'utils.php';
 
 //check if user is logged in
 ensure_session();
-if ($_GET['user'] != $_SESSION['user']) {
-    header('Location: /profilo/' . $_GET['user']);
-    exit();
+if ($_GET['user'] === $_SESSION['user']) {
+    
 }
 
 $page = getTemplatePage("Lista dei desideri");

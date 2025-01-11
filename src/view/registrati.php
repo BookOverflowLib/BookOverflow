8accaf1ea5e76c5714360b53e5e74ef2821b4f84
Alessandro Bernardello <53372753+alessandroberna@users.noreply.github.com>
2025-01-11 23:48:56 +0100

    feat: basic server-side error checking for user registration. TODO: insert error messages in page

--- src/view/registrati.php
+++ src/view/registrati.php
@@ -12,4 +12,26 @@ $provinceList = optionProvince();
 $registrati = str_replace('<!-- [province] -->', $provinceList, $registrati);
 $page = str_replace('<!-- [content] -->', $registrati, $page);
 
+if (isset($_GET['error'])) {
+    // TODO
+    $errorMessage = '';
+    switch ($_GET['error']) {
+        case 'missing':
+            $errorMessage = '<p class="input-error-regular">Inserire tutti i campi richiesti</p>';
+            break;
+        case 'password-mismatch':
+            $errorMessage = '<p class="input-error-regular">Le password non corrispondono</p>';
+            break;
+        case 'username-taken':
+            $errorMessage = '<p class="input-error-regular">Username già in uso</p>';
+            break;
+        case 'email-taken':
+            $errorMessage = '<p class="input-error-regular">Email già in uso</p>';
+            break;
+        case 'generic':
+            $errorMessage = '<p class="input-error-regular">Errore generico</p>';
+            break;
+    }
+}
+
 echo $page;

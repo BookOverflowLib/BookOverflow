89b6f00ec5d2c2f2f0b8e2a57d9f24606e65f401
bazz333 <matteobazzan333@gmail.com>
2025-01-15 19:40:25 +0100

    feat: aggiunta visualizzazione libro

--- src/view/profilo.php
+++ src/view/profilo.php
@@ -3,26 +3,34 @@ require_once '../src/paths.php';
 require_once $GLOBALS['MODEL_PATH'] . 'dbAPI.php';
 require_once $GLOBALS['MODEL_PATH'] . 'utils.php';
 
-//se non è stato passato un id utente, reindirizza alla home
+//se non è stato passato un id utente, reindirizza al tuo, se invece non sei loggato, ad accedi
 if (!isset($_GET['user'])) {
-	header('Location: /accedi');
-	exit();
+	if (!isset($_SESSION['user'])) {
+		header('Location: /accedi');
+		exit();
+	} else {
+		$_GET['user'] = $_SESSION['user'];
+	}
 }
 
 $profileId = $_GET['user'];
 
 ensure_session();
 
-$isTuoProfilo = isset($_SESSION['user']) ? ($_GET['user'] == $_SESSION['user']) : false;
+$isTuoProfilo = isset($_SESSION['user']) && $_GET['user'] == $_SESSION['user'];
 
 // USER DATA
 $db = new DBAccess();
-$dbOK = $db->open_connection();
+try {
+	$dbOK = $db->open_connection();
+} catch (Exception $e) {
+	$_SESSION['error'] = "Errore durante la connessione al database";
+}
 
 $user = $db->get_user_by_username($profileId);
 
-if (!$user || $user == null) {
-	// throw new Exception("Utente non trovato");
+if ($user == null) {
+	$_SESSION['error'] = "Utente non trovato";
 	header('Location: /404');
 	exit();
 }
@@ -45,8 +53,8 @@ foreach ($sostituzioni as $placeholder => $value) {
 }
 
 // Località
-$location = $db->get_provincia_comune_by_ids($user['provincia'], $user['comune']);
-$profilo = str_replace('<!-- [userLuogo] -->', $location['comune'] . ', ' . $location['provincia'], $profilo);
+$location = getLocationName($user['provincia'], $user['comune']);
+$profilo = str_replace('<!-- [userLuogo] -->', $location, $profilo);
 
 // Valutazione utente
 $userRating = $db->get_user_rating_by_email($user['email']);
@@ -64,10 +72,10 @@ $generi = $db->get_generi_by_username($user['username']);
 $profilo = str_replace('<!-- [userGeneri] -->', getGeneriPreferiti($generi), $profilo);
 
 $libri_offerti_db = $db->get_libri_offerti_by_username($user['username']);
-$profilo = str_replace('<!-- [libriOffertiLista] -->', getLibriCopertinaGrande($libri_offerti_db,4), $profilo);
+$profilo = str_replace('<!-- [libriOffertiLista] -->', getLibriCopertinaGrande($libri_offerti_db, 4), $profilo);
 
 $libri_desiderati_db = $db->get_libri_desiderati_by_username($user['username']);
-$profilo = str_replace('<!-- [libriDesideratiLista] -->', getLibriCopertinaGrande($libri_desiderati_db,4), $profilo);
+$profilo = str_replace('<!-- [libriDesideratiLista] -->', getLibriCopertinaGrande($libri_desiderati_db, 4), $profilo);
 
 // Se è il proprio profilo
 // aggiungi pulsanti per modifica profilo e generi
@@ -81,14 +89,14 @@ if ($isTuoProfilo) {
 	//libri offerti
 	$libriOffertiButton = '<a href="/profilo/' . $user['username'] . '/libri-offerti" class="button-layout">Modifica la lista</a>';
 	$profilo = str_replace('<!-- [libriOffertiButton] -->', $libriOffertiButton, $profilo);
-	
+
 	$libriDesideratiButton = '<a href="/profilo/' . $user['username'] . '/libri-desiderati" class="button-layout">Modifica la lista</a>';
 	$profilo = str_replace('<!-- [libriDesideratiButton] -->', $libriDesideratiButton, $profilo);
-	
+
 } else {
 	$profilo = str_replace('<!-- [logoutButton] -->', '', $profilo);
 	$profilo = str_replace('<!-- [generiPreferitiButton] -->', '', $profilo);
-	
+
 	// TODO: aggiungere aria-label cose blabla
 	$libriOffertiButton = '<a href="/profilo/' . $user['username'] . '/libri-offerti" class="button-layout">Mostra tutti</a>';
 	$profilo = str_replace('<!-- [libriOffertiButton] -->', $libriOffertiButton, $profilo);

21d27a7a25870f81c5a80f4557d7a6ec7f8c2737
Alessandro Bernardello <53372753+alessandroberna@users.noreply.github.com>
2025-01-16 15:30:10 +0100

    feat: make the login fields accept both emails and usernames

--- src/model/accesso-utente.php
+++ src/model/accesso-utente.php
@@ -7,14 +7,14 @@ ensure_session();
 $db = new DBAccess();
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    if (isset($_POST['email'], $_POST['password'])) {
-        $email = $_POST['email'];
+    if (isset($_POST['identifier'], $_POST['password'])) {
+        $identifier = $_POST['identifier'];
         $password = $_POST['password'];
 
         try {
-            $result = $db->login_user($email, $password);
+            $result = $db->login_user($identifier, $password);
             if ($result) {
-                $user = ($db->get_user_by_email($email))[0];
+                $user = ($db->get_user_by_identifier($identifier))[0];
                 $_SESSION['user'] = $user['username'];
                 $_SESSION['path_immagine'] = $user['path_immagine'];
                 header('Location: /profilo/' . $user['username']);

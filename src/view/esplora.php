66eb61c938c282de610d31fc1e177fea407ecdeb
bazz333 <matteobazzan333@gmail.com>
2025-01-17 19:55:00 +0100

    feat: ora forse gli scambi funzionano (no ancora api) feat: aggiunta pagina esplora con match per te e potrebbe piacerti

--- src/view/esplora.php
+++ src/view/esplora.php
@@ -3,11 +3,21 @@ require_once '../src/paths.php';
 require_once $GLOBALS['MODEL_PATH'] . 'dbAPI.php';
 require_once $GLOBALS['MODEL_PATH'] . 'utils.php';
 
+ensure_session();
+// per il momento solo utenti loggati possono accedere a questa pagina
+ensure_login();
+
 $db = new DBAccess();
 $dbOK = $db->open_connection();
 
 $page = getTemplatePage("Esplora");
 $esplora = file_get_contents($GLOBALS['TEMPLATES_PATH'] . 'esplora.html');
+
+$match_per_te = $db->get_match_per_te_by_user($_SESSION['user']);
+$esplora = str_replace('<!-- [matchPerTe] -->', getLibriCopertinaGrande($match_per_te, 4), $esplora);
+
+$match_potrebbe_piacerti = $db->get_potrebbe_piacerti_by_user($_SESSION['user']);
+$esplora = str_replace('<!-- [matchPotrebbePiacerti] -->', getLibriCopertinaGrande($match_potrebbe_piacerti, 4), $esplora);
 
 $page = str_replace('<!-- [content] -->', $esplora, $page);
 echo $page;

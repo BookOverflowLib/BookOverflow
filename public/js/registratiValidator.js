fd8102dd2f75fd4f18d508260e71782ab748f8a2
bazz333 <matteobazzan333@gmail.com>
2025-01-18 16:58:57 +0100

    fix: aggiunto suggerimento "user"

--- public/js/registratiValidator.js
+++ public/js/registratiValidator.js
@@ -6,38 +6,38 @@ import {checkForm, fillSuggestion} from './formValidator.js';
 // [3]: isValid
 var formChecks = {
     nome: [
-        "Ex: Mario",
+        "Es: Mario",
         /^[A-Za-z\s']{2,50}$/,
         "Inserire un nome di lunghezza almeno 2 e massimo 50, non sono ammessi numeri o caratteri speciali",
         false
     ],
     cognome: [
-        "Ex: Rossi",
+        "Es: Rossi",
         /^[A-Za-z\s']{2,50}$/,
         "Inserire un cognome di lunghezza almeno 2 e massimo 50, non sono ammessi numeri o caratteri speciali",
         false
     ],
     // all characters are allowed
     username: [
-        "Ex: MarioRossi",
+        "Es: MarioRossi",
         /^[^\s\r\n]{2,50}$/,
         "Inserire un username di lunghezza almeno 2 e massimo 50, sono ammessi caratteri speciali e numeri",
         false
     ],
     email: [
-        "Ex: mariorossi@gmail.com",
+        "Es: mariorossi@gmail.com",
         /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
         "Inserire un'email valida",
         false
     ],
     password: [
-        "Ex: Password123!",
+        "Es: Password123!",
         /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()\-+=])[A-Za-z\d!@#$%^&*()\-+=]{8,50}$/,
         "La password deve essere lunga tra 8 e 50 caratteri, contenere almeno una lettera maiuscola (A-Z), una lettera minuscola (a-z), un numero (0-9) e un carattere speciale tra i seguenti: !@#$%^&*()-+=.",
         false
     ],
     conferma: [
-        "Ex: Password123!",
+        "Es: Password123!",
         /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()\-+=])[A-Za-z\d!@#$%^&*()\-+=]{8,50}$/,
         "La password deve essere lunga tra 8 e 50 caratteri, contenere almeno una lettera maiuscola (A-Z), una lettera minuscola (a-z), un numero (0-9) e un carattere speciale tra i seguenti: !@#$%^&*()-+=.",
         false

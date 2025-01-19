907266d036fdb8e5a06dc89a9b7e1a5c87ff8885
bazz333 <matteobazzan333@gmail.com>
2025-01-19 18:41:01 +0100

    feat: implement session storage for form inputs and improve comune selection

--- public/js/comuneSelect.js
+++ public/js/comuneSelect.js
@@ -1,19 +1,29 @@
 let provinciaSelect = document.querySelector('select[name="provincia"]')
+
 if (provinciaSelect) {
     provinciaSelect.addEventListener('change', function () {
-        //find target Select menu
-        let cittaSelect = document.querySelector('select[name="comune"]')
-
-        //create payload to send to server
-        let fd = new FormData()
-        fd.set('provinciaSelezionata', this.value)
-
-        // send the ajax request using fetch
-        fetch('/api/ottieni-comuni', {method: 'post', body: fd})
-            .then(response => response.json())
-            .then(json => {
-                let options = '<option value="">Seleziona un comune</option><hr />'
-                cittaSelect.innerHTML = options + json.comuni
-            })
+        getComuniByProvincia(this.value)
     })
+}
+
+
+function getComuniByProvincia(provincia) {
+    let cittaSelect = document.querySelector('select[name="comune"]')
+
+    //create payload to send to server
+    let fd = new FormData()
+    fd.set('provinciaSelezionata', provincia)
+
+    // send the ajax request using fetch
+    fetch('/api/ottieni-comuni', {method: 'post', body: fd})
+        .then(response => response.json())
+        .then(json => {
+            let options = '<option value="">Seleziona un comune</option><hr />'
+            cittaSelect.innerHTML = options + json.comuni
+        })
+        .then(() => {
+            if (sessionStorage.getItem("comune")) {
+                cittaSelect.value = sessionStorage.getItem("comune")
+            }
+        })
 }

549a9f7eb3d3a16301cf03ccf5f02efae7bd964c
lucaribon <lucaribon0203@gmail.com>
2025-01-22 01:06:25 +0100

    fix: suggerimento iniziale form come placeholder; closes #93

--- public/js/formValidator.js
+++ public/js/formValidator.js
@@ -27,26 +27,29 @@ function setSuggestion(input, mode, formChecks) {
 
     switch (mode) {
         case 0:
-            newNode.textContent = formChecks[input.id][0];
-            newNode.className = "input-hint";
+            input.placeholder = formChecks[input.id][0];
             break;
         case 1:
             newNode.textContent = formChecks[input.id][2];
             newNode.className = "input-error";
+            //parent.insertBefore(newNode, parent.lastChild); //TODO: maybe after is better?
+            parent.insertBefore(newNode, parent.lastChild.previousSibling);
             break;
         case 2:
             newNode.textContent = "Campo obbligatorio";
             newNode.className = "input-error";
+            //parent.insertBefore(newNode, parent.lastChild); //TODO: maybe after is better?
+            parent.insertBefore(newNode, parent.lastChild.previousSibling);
             break;
         case 3:
             newNode.textContent = "Le password non corrispondono";
             newNode.className = "input-error";
+            //parent.insertBefore(newNode, parent.lastChild); //TODO: maybe after is better?
+            parent.insertBefore(newNode, parent.lastChild.previousSibling);
             break;
         default:
             break;
     }
-    //parent.insertBefore(newNode, parent.lastChild); //TODO: maybe after is better?
-    parent.insertBefore(newNode, parent.lastChild.previousSibling);
 }
 
 function checkRegex(input, formChecks) {

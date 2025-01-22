b4d391bad91c074bce079853059ab4b6318194ad
bazz333 <matteobazzan333@gmail.com>
2025-01-22 11:41:14 +0100

    feat: mostra errori in /accedi

--- src/view/accedi.php
+++ src/view/accedi.php
@@ -4,9 +4,9 @@ require_once $GLOBALS['MODEL_PATH'] . 'utils.php';
 
 ensure_session();
 
-if(isset($_SESSION['user'])) {
-    header('Location: /profilo/' . $_SESSION['user']);
-    exit();
+if (isset($_SESSION['user'])) {
+	header('Location: /profilo/' . $_SESSION['user']);
+	exit();
 }
 
 require_once '../src/paths.php';
@@ -18,21 +18,18 @@ $accedi = file_get_contents($GLOBALS['TEMPLATES_PATH'] . 'accedi.html');
 
 // Handle error messages
 if (isset($_GET['error'])) {
-    $errorMessage = '';
-    switch ($_GET['error']) {
-        case 'wrong-password':
-            $errorMessage = '<p class="input-error-regular">Le credenziali inserite non sono valide</p>';
-            break;
-        case 'missing':
-            $errorMessage = '<p class="input-error-regular">Inserire tutti i campi richiesti</p>';
-            break;
-    }
-    // Insert error message after the h1
-    $accedi = str_replace(    
-        '</h1>',
-        '</h1>' . $errorMessage,
-        $accedi
-    );
+	$errorMessage = '';
+	switch ($_GET['error']) {
+		case 'invalid':
+			$errorMessage = '<p class="input-error-regular">Le credenziali inserite non sono valide</p>';
+			break;
+		case 'missing':
+			$errorMessage = '<p class="input-error-regular">Inserire tutti i campi richiesti</p>';
+			break;
+	}
+
+	// Insert error message in box
+	$accedi = str_replace('<!-- [displayError] -->', $errorMessage, $accedi);
 }
 
 $page = str_replace('<!-- [content] -->', $accedi, $page);

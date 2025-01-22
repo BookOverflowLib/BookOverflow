067dd918acd096a726faeff8ca92f0b115655b70
bazz333 <matteobazzan333@gmail.com>
2025-01-22 15:05:46 +0100

    fix: invertiti pulsanti conferma/annulla

--- src/model/utils.php
+++ src/model/utils.php
@@ -23,23 +23,23 @@ function ratingStars($rating): string
 	if ($rating > 5 || $rating < 0) {
 		throw new Exception('Rating non nei vincoli', 1);
 	}
-
+	
 	$n_full_star = floor($rating); //PARTE INTERA
 	$n_partial_star = $rating - $n_full_star; //PARTE FRAZIONARIA
 	$star_svg = file_get_contents('../public/assets/imgs/star.svg');
-
+	
 	$total_star = 5;
 	$rating_stars = '';
-
+	
 	// STELLE PIENE
 	for ($i = 0; $i < $n_full_star; $i++) {
 		$tmp_star = str_replace('{{star-offset}}', '100', $star_svg);
 		$tmp_star = str_replace('{{id}}', '1', $tmp_star);
-
+		
 		$rating_stars .= $tmp_star;
 		$total_star--;
 	}
-
+	
 	// STELLA PERCENTUALE
 	$par_star = str_replace(
 		'{{star-offset}}',
@@ -49,7 +49,7 @@ function ratingStars($rating): string
 	$par_star = str_replace('{{id}}', '2', $par_star);
 	$rating_stars .= $par_star;
 	$total_star--;
-
+	
 	//STELLE VUOTE
 	while ($total_star > 0) {
 		$tmp_star = str_replace('{{star-offset}}', '0', $star_svg);
@@ -57,7 +57,7 @@ function ratingStars($rating): string
 		$rating_stars .= $tmp_star;
 		$total_star--;
 	}
-
+	
 	return $rating_stars;
 }
 
@@ -77,20 +77,20 @@ function getTemplatePage($title = null): string
 	$header = getHeaderSection($_SERVER['REQUEST_URI']);
 	$breadcrumb = getBreadcrumb($_SERVER['REQUEST_URI']);
 	$footer = file_get_contents($GLOBALS['TEMPLATES_PATH'] . 'footer.html');
-
+	
 	$PAGE_TITLE = '';
 	if ($title == null) {
 		$PAGE_TITLE = 'BookOverflow';
 	} else {
 		$PAGE_TITLE = ucfirst($title) . ' - BookOverflow';
 	}
-
+	
 	$page = str_replace('<!-- [pageTitle] -->', $PAGE_TITLE, $template);
-
+	
 	$page = str_replace('<!-- [header] -->', $header, $page);
 	$page = str_replace('<!-- [breadcrumb] -->', $breadcrumb, $page);
 	$page = str_replace('<!-- [footer] -->', $footer, $page);
-
+	
 	return $page;
 }
 
@@ -102,13 +102,13 @@ function getTemplatePage($title = null): string
 function getNavBarLi($path): string
 {
 	$currentPage = $path;
-
+	
 	$navbarReferences = [
 		['href' => '/', 'text' => 'Home'],
 		['href' => '/esplora', 'text' => 'Esplora'],
 		['href' => '/come-funziona', 'text' => 'Come funziona'],
 	];
-
+	
 	$li = '';
 	foreach ($navbarReferences as $ref) {
 		if ($currentPage != $ref['href']) {
@@ -136,7 +136,7 @@ function getHeaderButtons($path): string
 		$chiara .
 		$scura .
 		'</button>';
-
+	
 	// Se la pagina corrente è /accedi, il pulsante deve portare a /registrati
 	$accediButton = '';
 	if ($path != '/accedi') {
@@ -145,10 +145,10 @@ function getHeaderButtons($path): string
 		$accediButton =
 			'<a class="button-layout" href="/registrati">Registrati</a>';
 	}
-
+	
 	$ris = '';
 	ensure_session();
-
+	
 	if (isset($_SESSION['user'])) {
 		$ris = <<<HTML
 			<div class="header-buttons">
@@ -165,7 +165,7 @@ function getHeaderButtons($path): string
 			$accediButton .
 			'</div>';
 	}
-
+	
 	return $ris;
 }
 
@@ -181,7 +181,7 @@ function getHamburgerButton(): string
 		$chiuso .
 		$aperto .
 		'</button>';
-
+	
 	return $hamburgerIcon;
 }
 
@@ -193,7 +193,7 @@ function getHamburgerButton(): string
 function getHeaderSection($path): string
 {
 	$header = file_get_contents('../src/templates/header.html');
-
+	
 	$myHeader = str_replace('<!-- [navbar] -->', getNavBarLi($path), $header);
 	$myHeader = str_replace(
 		'<!-- [header-buttons] -->',
@@ -205,7 +205,7 @@ function getHeaderSection($path): string
 		getHamburgerButton(),
 		$myHeader
 	);
-
+	
 	return $myHeader;
 }
 
@@ -238,14 +238,14 @@ function getBreadcrumb($path): string
 			if ($currentPath !== 'Profilo' && $currentPath !== 'Libro') {
 				$elements .= '<li><a href="' . $currentUrl . '">' . $currentPath . '</a></li>';
 			}
-
+			
 		}
 		$currentPath = str_replace('-', ' ', ucfirst($path[$i]));
-
+		
 		if (isset($_SESSION['user']) && $i > 0 && $path[$i - 1] == 'profilo' && $path[$i] == $_SESSION['user']) {
 			$currentPath = 'Il mio profilo';
 		}
-
+		
 		// Check if the URL is /libro/{isbn} and replace it with /{titolo-libro}
 		if ($i > 0 && $path[$i - 1] == 'libro') {
 			$isbn = $path[$i];
@@ -254,13 +254,13 @@ function getBreadcrumb($path): string
 			$bookTitle = $bookTitle[0]['titolo'];
 			$currentPath = ucfirst($bookTitle);
 		}
-
+		
 		if ($currentPath !== 'Profilo') {
 			$elements .= '<li aria-current="page" class="bold">' . $currentPath . '</li>';
 		}
 	}
 	$breadcrumb = "<ol>$elements</ol>";
-
+	
 	return $breadcrumb;
 }
 
@@ -273,17 +273,17 @@ function getBreadcrumb($path): string
 function getUserImageUrlByEmail($email): string
 {
 	$image = 'https://picsum.photos/seed/' . $email . '/500';
-
+	
 	$ch = curl_init();
 	curl_setopt($ch, CURLOPT_URL, $image);
 	curl_setopt($ch, CURLOPT_HEADER, true); // true to include the header in the output.
 	curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true); // Must be set to true true to follow any "Location: " header that the server sends as part of the HTTP header.
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); // true to return the transfer as a string of the return value of curl_exec() instead of outputting it directly.
-
+	
 	$a = curl_exec($ch); // $a will contain all headers
-
+	
 	$finalUrl = curl_getinfo($ch, CURLINFO_EFFECTIVE_URL); // This is what you need, it will return you the last effective URL
-
+	
 	return $finalUrl; // Voila
 }
 
@@ -331,10 +331,10 @@ function getGeneriPreferiti($generi)
 	) {
 		return '<p>Non c\'è ancora nessun genere preferito!</p>';
 	}
-
+	
 	$fileGeneri = json_decode(file_get_contents('../utils/bisac.json'), true);
 	$generiPreferiti = json_decode($generi[0]['generi_preferiti'], true);
-
+	
 	$output = '';
 	foreach ($generiPreferiti as $genereKey) {
 		$genere = $fileGeneri[$genereKey];
@@ -386,8 +386,8 @@ function getLibriList($libri_utente, $list_name): string
 	if ($list_name != 'libri-offerti' && $list_name != 'libri-desiderati') {
 		throw new TypeError("list_name deve essere 'libri-desiderati' o 'libri-offerti'");
 	}
-
-
+	
+	
 	$libri_html = '';
 	if (!$libri_utente) {
 		$libri_html = '<p>Non ci sono ancora libri qui!</p>';
@@ -397,7 +397,7 @@ function getLibriList($libri_utente, $list_name): string
 			$titolo = $libro['titolo'];
 			$autore = $libro['autore'];
 			$path_copertina = $libro['path_copertina'];
-
+			
 			$book_copy_info = '';
 			if ($list_name === 'libri-offerti') {
 				$condizioni = ucfirst($libro['condizioni']);
@@ -411,13 +411,13 @@ function getLibriList($libri_utente, $list_name): string
 				<p>Condizioni: {$condizioni}</p>
 				HTML;
 			}
-
+			
 			if (isTuoProfilo($_GET['user'])) {
 				$bookButtons = getLibriListBookButtons($list_name, $isbn, $titolo);
 			} else {
 				$bookButtons = '';
 			}
-
+			
 			$libroRowTemplate = <<<HTML
 			<div class="book-row">
 				<div class="book-info">
@@ -453,7 +453,7 @@ function isTuoProfilo($profileId)
 
 function getLibriListBookButtons($list_name, $isbn, $titolo)
 {
-
+	
 	$api = $list_name === 'libri-offerti' ? '/api/rimuovi-libro-offerto' : '/api/rimuovi-libro-desiderato';
 	$bookButtons = <<<HTML
 	<form action="{$api}" method="post">
@@ -476,13 +476,13 @@ function addButtonsLibriList($libri_page, $list_name): string
 	if ($list_name != 'libri-offerti' && $list_name != 'libri-desiderati') {
 		throw new TypeError("list_name deve essere 'libri-desiderati' o 'libri-offerti'");
 	}
-
+	
 	$form_action = $list_name === 'libri-offerti' ? '/api/aggiungi-libro-offerto' : '/api/aggiungi-libro-desiderato';
-
+	
 	$libri_utente = $libri_page;
 	$aggiungiLibro = '<button class="button-layout" id="aggiungi-libro-button" aria-label="Aggiungi un libro ai libri offerti">Aggiungi un libro</button>';
 	$libri_utente = str_replace('<!-- [aggiungiLibroButton] -->', $aggiungiLibro, $libri_utente);
-
+	
 	$select_condizioni = '';
 	if ($list_name === 'libri-offerti') {
 		$select_condizioni = <<<HTML
@@ -500,7 +500,7 @@ function addButtonsLibriList($libri_page, $list_name): string
 		</div>
 		HTML;
 	}
-
+	
 	$cercaLibriDialog = <<<HTML
 	<dialog id="aggiungi-libro-dialog">
 		<div class="dialog-window">
@@ -528,8 +528,8 @@ function addButtonsLibriList($libri_page, $list_name): string
 				<input type="hidden" name="lingua" value="">
 				<input type="hidden" name="path_copertina" value="">
 				<div class="dialog-buttons">
-					<button id="close-dialog" class="button-layout-light" type="reset" formnovalidate>Annulla</button>
 					<input type="submit" id="aggiungi-libro" class="button-layout" value="Aggiungi libro">
+					<button id="close-dialog" class="button-layout-light" type="reset" formnovalidate>Annulla</button>
 				</div>
 			</form>
 		</div>

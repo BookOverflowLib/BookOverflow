14fef808ca3283592f2b1e5e7f15e9e47b20d6c9
lucaribon <lucaribon0203@gmail.com>
2025-01-24 17:29:14 +0100

    fix: sistemata tabella recensione db e query get_user_rating_by_email;

--- db/tables.sql
+++ db/tables.sql
@@ -92,8 +92,8 @@ CREATE TABLE
 
 CREATE TABLE
     Recensione (
-        -- email del recensito derivata dalla tabella scambio
-        emailRecensore VARCHAR(255),
+        -- email del recensore derivata dalla tabella scambio
+        emailRecensito VARCHAR(255),
         idScambio INT,
         dataPubblicazione DATE DEFAULT CURRENT_DATE,
         valutazione TINYINT UNSIGNED CHECK (
@@ -101,9 +101,9 @@ CREATE TABLE
             AND valutazione <= 5
         ),
         contenuto TEXT, -- 65k caratteri
-        FOREIGN KEY (emailRecensore) REFERENCES Utente (email),
+        FOREIGN KEY (emailRecensito) REFERENCES Utente (email),
         FOREIGN KEY (idScambio) REFERENCES Scambio (ID),
-        PRIMARY KEY (emailRecensore, idScambio)
+        PRIMARY KEY (emailRecensito, idScambio)
     );
 
 CREATE TABLE

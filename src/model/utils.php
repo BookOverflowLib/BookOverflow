4eb1bb269aa8e7fc2f0878248da68b0ffa7c93ed
Alessandro Bernardello <53372753+alessandroberna@users.noreply.github.com>
2025-01-24 02:08:25 +0100

    fix: correct file path for star SVG in ratingStars function

--- src/model/utils.php
+++ src/model/utils.php
@@ -43,10 +43,9 @@ function ratingStars($rating): string
 	if ($rating > 5 || $rating < 0) {
 		throw new Exception('Rating non nei vincoli', 1);
 	}
-	
 	$n_full_star = floor($rating); //PARTE INTERA
 	$n_partial_star = $rating - $n_full_star; //PARTE FRAZIONARIA
-	$star_svg = file_get_contents('../public./assets/imgs/star.svg');
+	$star_svg = file_get_contents('../public/assets/imgs/star.svg');
 	
 	$total_star = 5;
 	$rating_stars = '';
@@ -147,6 +146,7 @@ function getNavBarLi($path): string
 
 function getHeaderButtons($path): string
 {
+	$prefix = getPrefix();
 	$scura =
 		'<span class="active"><img class="theme-icon" src="' . $prefix . '/assets/imgs/moon.svg" alt="" aria-hidden="true"><span class="visually-hidden">Modalità scura</span></span>';
 	$chiara =

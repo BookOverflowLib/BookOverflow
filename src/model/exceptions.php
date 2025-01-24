9dfbd87f5a8d028066025b4e249783b365ed7c8d
Alessandro Bernardello <53372753+alessandroberna@users.noreply.github.com>
2025-01-24 18:51:10 +0100

    feat: custom exceptions, streamlined error handling

---
+++ src/model/exceptions.php
@@ -0,0 +1,48 @@
+<?php
+// exception con messaggi personalizzati per ridurre la duplicazione di codice
+
+namespace CustomExceptions;
+use Exception;
+
+// consente di fare fare throw solo di exception personalizzate dopo un catch
+class CustomException extends Exception {
+    public function __construct($message) {
+        parent::__construct($message);
+    }
+}
+
+class UsernameAlreadyExistsException extends CustomException {
+    public function __construct($username) {
+        parent::__construct("L'utente $username è già registrato");
+    }
+}
+
+class EmailAlreadyExistsException extends CustomException {
+    public function __construct($email) {
+        parent::__construct("L'email $email è già associata ad un account");
+    }
+}
+
+class IncorrectCredentialsException extends CustomException {
+    public function __construct() {
+        parent::__construct("Le credenziali inserite non sono corrette");
+    }
+}
+
+class GenericRegistrationException extends CustomException {
+    public function __construct() {
+        parent::__construct("Errore durante la registrazione");
+    }
+}
+
+class GenericCustomException_Alt extends CustomException {
+    public function __construct($article, $action) {
+        parent::__construct("Errore durante " . $article . $action);
+    }
+}
+
+class GenericCustomException extends CustomException {
+    public function __construct($text) {
+        parent::__construct("Errore: " . $text);
+    }
+}

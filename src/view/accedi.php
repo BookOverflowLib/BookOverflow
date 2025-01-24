dc0211d5b1a7a0db6a2614c873fa6dcb7aa3d62c
Alessandro Bernardello <53372753+alessandroberna@users.noreply.github.com>
2025-01-24 16:13:12 +0100

    feat: save login/registration errors in session

--- src/view/accedi.php
+++ src/view/accedi.php
@@ -16,22 +16,7 @@ require_once $GLOBALS['MODEL_PATH'] . 'utils.php';
 $page = getTemplatePage('Accedi');
 $accedi = file_get_contents($GLOBALS['TEMPLATES_PATH'] . 'accedi.html');
 
-// Handle error messages
-if (isset($_GET['error'])) {
-	$errorMessage = '';
-	switch ($_GET['error']) {
-		case 'invalid':
-			$errorMessage = '<p class="input-error-regular">Le credenziali inserite non sono valide</p>';
-			break;
-		case 'missing':
-			$errorMessage = '<p class="input-error-regular">Inserire tutti i campi richiesti</p>';
-			break;
-	}
-
-	// Insert error message in box
-	$accedi = str_replace('<!-- [displayError] -->', $errorMessage, $accedi);
-}
-
 $page = str_replace('<!-- [content] -->', $accedi, $page);
+$page = addErrorsToPage($page);
 $page = populateWebdirPrefixPlaceholders($page);
 echo $page;

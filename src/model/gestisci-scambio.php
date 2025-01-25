6d787e4a2ff48126de1179b4de20c412b5322d16
bazz333 <matteobazzan333@gmail.com>
2025-01-25 15:47:52 +0100

    feat: unifica la gestione degli scambi in un unico modulo

---
+++ src/model/gestisci-scambio.php
@@ -0,0 +1,61 @@
+<?php
+require_once __DIR__ . '/' . '../paths.php';
+require_once $GLOBALS['MODEL_PATH'] . 'dbAPI.php';
+require_once $GLOBALS['MODEL_PATH'] . 'utils.php';
+
+ensure_session();
+$db = new DBAccess();
+
+const VALID_ENDPOINTS = [
+	'/api/accetta-scambio',
+	'/api/rifiuta-scambio',
+	'/api/rimuovi-scambio'
+];
+
+if ($_SERVER["REQUEST_METHOD"] !== "POST") {
+	redirect("Errore: richiesta non valida");
+}
+if (!isset($_SESSION['user'])) {
+	redirect('Errore: utente non autenticato');
+}
+if (!isset($_POST['id_scambio'])) {
+	redirect('Errore: id_scambio non specificato');
+}
+if (!is_numeric($_POST['id_scambio'])) {
+	redirect("Errore: id_scambio non valido");
+}
+if (!in_array($_SERVER['REQUEST_URI'], VALID_ENDPOINTS, true)) {
+	redirect("Errore: endpoint non valido");
+}
+
+$id = (int) $_POST['id_scambio'];
+
+try {
+	switch ($_SERVER['REQUEST_URI']) {
+		case '/api/accetta-scambio':
+			$db->accetta_scambio_by_id($id);
+			break;
+		case '/api/rifiuta-scambio':
+			$db->rifiuta_scambio_by_id($id);
+			break;
+		case '/api/rimuovi-scambio':
+			$db->remove_scambio_by_id($id);
+			break;
+		default:
+			redirect("Errore: endpoint non valido");
+	}
+
+} catch (Exception $e) {
+	$err = exceptionToError($e, "scambio non riuscito");
+	redirect($err);
+}
+
+function redirect(string $error = null): never
+{
+	if ($error) {
+		$_SESSION['error'] = $error;
+	}
+	$previousUrl = parse_url($_SERVER['HTTP_REFERER'] ?? $GLOBALS['prefix'] . '/', PHP_URL_PATH);
+	header('Location: ' . $previousUrl);
+	exit();
+}

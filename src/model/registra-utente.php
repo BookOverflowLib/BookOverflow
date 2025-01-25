44b1c92b4dc8fb39e80b21c2e655e0bd1f04a90d
Alessandro Bernardello <53372753+alessandroberna@users.noreply.github.com>
2025-01-25 18:09:30 +0100

    fix: missing path_immagine after registration

--- src/model/registra-utente.php
+++ src/model/registra-utente.php
@@ -46,6 +46,7 @@ if (!preg_match("/^[^\s\r\n]{2,50}$/", $username)) {
 try {
 	$db->register_user($nome, $cognome, $id_provincia, $id_comune, $email, $username, $password, $image);
 	$_SESSION['user'] = $username;
+	$_SESSION['path_immagine'] = $image;
 	redirect();
 } catch (Exception $e) {
 	$err = exceptionToError($e, "registrazione non riuscita");

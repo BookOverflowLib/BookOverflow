cdf94d5a61057edffcf232ff53827df1e082942f
bazz333 <matteobazzan333@gmail.com>
2025-01-25 15:22:05 +0100

    feat: miglioramenti alla validazione e gestione degli accessi utente

--- src/model/accesso-utente.php
+++ src/model/accesso-utente.php
@@ -6,29 +6,33 @@ require_once __DIR__ . '/' . '../model/registration-select.php';
 ensure_session();
 $db = new DBAccess();
 
-$prefix=getPrefix();
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    if (isset($_POST['identifier'], $_POST['password'])) {
-        $identifier = $_POST['identifier'];
-        $password = $_POST['password'];
-
-        try {
-            $result = $db->login_user($identifier, $password);
-            if ($result) {
-                $user = ($db->get_user_by_identifier($identifier))[0];
-                $_SESSION['user'] = $user['username'];
-                $_SESSION['path_immagine'] = $user['path_immagine'];
-                header('Location: ' . $prefix . '/profilo/' . $user['username']);
-                exit();
-            }
-        } catch (Exception $e) {
-            header('Location: ' . $prefix . '/accedi');
-            $_SESSION['error'] = exceptionToError($e, "accesso non riuscito");
-            exit();
-        }
-    }
+$prefix = getPrefix();
+if ($_SERVER["REQUEST_METHOD"] !== "POST") {
+    $_SESSION['error'] = "Errore: richiesta non valida";
+    header('Location: ' . $prefix . '/accedi');
+    exit();
 }
 
-$_SESSION['error'] = "Inserire tutti i campi richiesti";
-header('Location: ' . $prefix . '/accedi');
-exit();
+if (!isset($_POST['identifier'], $_POST['password'])) {
+    $_SESSION['error'] = "Errore: dati mancanti";
+    header('Location: ' . $prefix . '/accedi');
+    exit();
+}
+
+$identifier = $_POST['identifier'];
+$password = $_POST['password'];
+
+try {
+    $result = $db->login_user($identifier, $password);
+    if ($result) {
+        $user = ($db->get_user_by_identifier($identifier))[0];
+        $_SESSION['user'] = $user['username'];
+        $_SESSION['path_immagine'] = $user['path_immagine'];
+        header('Location: ' . $prefix . '/profilo/' . $user['username']);
+        exit();
+    }
+} catch (Exception $e) {
+    header('Location: ' . $prefix . '/accedi');
+    $_SESSION['error'] = exceptionToError($e, "accesso non riuscito");
+    exit();
+}

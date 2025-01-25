1ad58b204dd153b16e63429fd31bdd1550079134
Alessandro Bernardello <53372753+alessandroberna@users.noreply.github.com>
2025-01-25 17:14:02 +0100

    refactor: use getPrefix()

--- src/model/aggiorna-generi.php
+++ src/model/aggiorna-generi.php
@@ -44,11 +44,12 @@ try {
 
 function redirect(string $error = null): never
 {
+	$prefix = getPrefix();
 	if ($error) {
 		$_SESSION['error'] = $error;
-		header('Location: ' . $GLOBALS['prefix'] . '/profilo/' . $_SESSION['user'] . '/seleziona-generi');
+		header('Location: ' . $prefix . '/profilo/' . $_SESSION['user'] . '/seleziona-generi');
 	}else{
-		header('Location: ' . $GLOBALS['prefix'] . '/profilo/' . $_SESSION['user'] . '#generi');
+		header('Location: ' . $prefix . '/profilo/' . $_SESSION['user'] . '#generi');
 	}
 	exit();
 }

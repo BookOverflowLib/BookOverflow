a502bb5bba2337a68ca7271d5ef26f552996d705
bazz333 <matteobazzan333@gmail.com>
2025-01-25 19:03:30 +0100

    feat: iniziato banner nuovo profilo

--- src/view/profilo.php
+++ src/view/profilo.php
@@ -165,6 +165,57 @@ function getContattaButton($profilo, $user)
 	return '<a href="' . $formatMailHref . '" class="button-layout secondary external-link">Contatta via mail</a>';
 }
 
+function getBannerNuovoProfilo($isTuoProfilo)
+{
+	if(!$isTuoProfilo) {
+		return '';
+	}
+	$icon = <<<HTML
+	<svg
+		xmlns="http://www.w3.org/2000/svg"
+		width="24"
+		height="24"
+		viewBox="0 0 24 24"
+		fill="none"
+		stroke="currentColor"
+		stroke-width="2"
+		stroke-linecap="round"
+		stroke-linejoin="round"
+		class="lucide lucide-notebook-pen">
+		<path
+			d="M13.4 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2v-7.4" />
+		<path d="M2 6h4" />
+		<path d="M2 10h4" />
+		<path d="M2 14h4" />
+		<path d="M2 18h4" />
+		<path
+			d="M21.378 5.626a1 1 0 1 0-3.004-3.004l-5.01 5.012a2 2 0 0 0-.506.854l-.837 2.87a.5.5 0 0 0 .62.62l2.87-.837a2 2 0 0 0 .854-.506z" />
+	</svg>
+	HTML;
+	$banner = <<<HTML
+	<div class="sezione-libri" id="completa-profilo">
+		<div class="message-box">
+			{$icon}
+			<div>
+				<h2>Completa il tuo profilo</h2>
+				<p class="center">Aggiungi subito:</p>
+				<ul>
+					<li>
+						i tuoi generi preferiti
+					</li>
+					<li>
+						i libri che offri
+					</li>
+					<li>
+						i libri che desideri
+					</li>
+				</ul>
+			</div>
+		</div>
+	</div>
+	HTML;
+}
+
 function redirect(string $error = null): never
 {
 	if ($error) {

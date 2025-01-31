4977f463b3beda27cecef59b8ff70d2e651ee1a2
bazz333 <matteobazzan333@gmail.com>
2025-01-31 11:28:01 +0100

    fix: some fixes DB dati e empty tag esplora cose

--- src/view/esplora-per-te.php
+++ src/view/esplora-per-te.php
@@ -15,10 +15,10 @@ $esplora = str_replace('<!-- [esploraTuttiTitolo] -->', 'Match per te', $esplora
 
 $sottotitolo = 'Scopri i match perfetti! Qui trovi i libri che desideri e che altri utenti offrono, la bella notizia è che anche tu hai qualcosa che loro desiderano!';
 $esplora = str_replace('<!-- [sottotitolo] -->', $sottotitolo, $esplora);
+$esplora = str_replace('<!-- [ricerca] -->', '', $esplora);
 
 $db = new DBAccess();
 $match_per_te = $db->get_match_per_te_by_user($_SESSION['user']);
-
 $esplora = str_replace('<!-- [caroselloTuttiLibri] -->', getLibriCopertinaGrande($match_per_te, 999), $esplora);
 
 $page = str_replace('<!-- [content] -->', $esplora, $page);

5736fcd8fec63716c1616e14f2f6f25e33dfb292
bazz333 <matteobazzan333@gmail.com>
2025-01-31 15:08:19 +0100

    refactor: update mobile styles and clean up form validation logic

--- js/gestioneUtenti.js
+++ js/gestioneUtenti.js
@@ -1,16 +1,32 @@
+
 const buttonsElimina = document.querySelectorAll('.elimina-utente');
 const sureDialog = document.getElementById('sure-dialog');
 
 let userClicked = '';
 buttonsElimina.forEach(button => {
-    button.addEventListener('click', () => {
-        document.getElementById('form-username').value = button.dataset.username
-        sureDialog.showModal()
-    })
+	button.addEventListener('click', () => {
+		document.getElementById('form-username').value = button.dataset.username
+		sureDialog.showModal()
+	})
 });
 
 const confermaElimina = document.getElementById('conferma-elimina')
 const closeButton = document.getElementById('close-dialog');
 closeButton.addEventListener('click', () => {
-    sureDialog.close();
+	sureDialog.close();
 });
+
+
+document.addEventListener('DOMContentLoaded', function () {
+	if (sessionStorage.getItem('nome')) {
+		clearInputValueSessionStorage();
+	}
+});
+
+function clearInputValueSessionStorage() {
+	var inputs = ['nome', 'cognome', 'provincia', 'comune', 'username', 'email'];
+
+	inputs.forEach(function (input) {
+		sessionStorage.removeItem(input);
+	});
+}
